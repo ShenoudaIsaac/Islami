69e569236fdc0e1254f141329184889da9977a39
shenoudaIsaac <shenoudaisaac54@gmail.com>
2025-02-03 01:55:21 +0200

    Edit Sebha Rotation

--- app/src/main/java/com/shnsh/islami/fragments/SebhaFragment.kt
+++ app/src/main/java/com/shnsh/islami/fragments/SebhaFragment.kt
@@ -1,17 +1,20 @@
 package com.shnsh.islami.fragments
 
+import android.graphics.Matrix
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.shnsh.islami.R
+import android.widget.ImageView.ScaleType
+import androidx.fragment.app.Fragment
 import com.shnsh.islami.databinding.FragmentSebhaBinding
+
 
 class SebhaFragment : Fragment() {
     lateinit var binding:FragmentSebhaBinding
 
     var counterr=0
+    var zekrr="سبحان الله"
    
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +26,41 @@ class SebhaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentSebhaBinding.inflate(layoutInflater)
+        binding=FragmentSebhaBinding.inflate(inflater,container,false)
         return binding.root
+
     }
+    var degrees= 150f
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.counter.text="0"
-        binding.counter.setOnClickListener {
-            counterr++
-            binding.counter.text=counterr.toString()
-        }
-    }
+        binding.counter.text =counterr.toString()
+        binding.zekr.text=zekrr
 
-}
+        binding.sebhaBody.setOnClickListener {
+
+            val matrix = Matrix()
+
+
+
+            binding.sebhaBody.setScaleType(ScaleType.MATRIX) //required
+            matrix.postRotate(
+                degrees,
+                (binding.sebhaBody.getDrawable().getBounds().width() / 2).toFloat(),
+                (binding.sebhaBody.getDrawable().getBounds().height() / 2).toFloat()
+            )
+            degrees=degrees+150
+            binding.sebhaBody.setImageMatrix(matrix)
+            counterr++
+            binding.counter.text = counterr.toString()
+            if (counterr % 33 == 0)
+                binding.zekr.text = "الحمد لله"
+            if (counterr % 66 == 0)
+                binding.zekr.text="استغفر الله"
+            if (counterr%99==0)
+                binding.zekr.text="سبحان الله"
+
+
+        }}}
+
+
